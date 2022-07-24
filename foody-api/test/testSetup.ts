997bd2fb996a0e0656bf75d30f9d471f805455ca
Hayelom Tizazu <hayelomtizazu19@gmail.com>
2022-07-24 07:20:39 +0300

    mocha testing setup

---
+++ foody-api/test/testSetup.ts
@@ -0,0 +1,23 @@
+import UserToken from '../src/core/fetures/user/usertoken/usertoken';
+
+const mongoUnit = require('mongo-unit');
+
+const clearDb = async () => {
+  await UserToken.deleteMany();
+  // await User.deleteMany();
+};
+
+before(async () => {
+  console.log('before globalizer');
+  await mongoUnit.start();
+  process.env.MONGODB_URI = mongoUnit.getUrl();
+});
+
+beforeEach(async () => {
+  console.log('before each');
+  await clearDb();
+});
+
+after(async () => {
+  await clearDb();
+});
